88d333c1d419ec68d50039bf12132dbb20eff252
Leandro Brangi <leandro.brangi@gmail.com>
2023-11-25 18:47:10 -0300

    fix importacion

--- src/components/tecnologies/tecnologies.tsx
+++ src/components/tecnologies/tecnologies.tsx
@@ -2,28 +2,40 @@ import js from "../../assets/JS.png";
 import ts from "../../assets/TS.png";
 import rc from "../../assets/RCT.png";
 import pg from "../../assets/PG.png";
-import fg from "../../assets/FG.png";
+import fg from "../../assets/fg.png";
 import style from "./tecnologies.module.css";
 
 export const Tecnologies = () => {
   return (
     <div>
-
       <div className={style.logos}>
         <div>
-          <button className={style.button}><img src={js} className={style.logo} /></button>
+          <button className={style.button}>
+            <img src={js} alt="JavaScript" className={style.logo} />
+          </button>
+          <div className={style.descripcion} id="descripcion">
+            Descripción de la imagen
+          </div>
         </div>
         <div>
-          <button className={style.button}><img src={ts} className={style.logo} /></button>
+          <button className={style.button}>
+            <img src={ts} className={style.logo} />
+          </button>
         </div>
         <div>
-        <button className={style.button}><img src={rc} className={style.logo} /></button>
+          <button className={style.button}>
+            <img src={rc} className={style.logo} />
+          </button>
         </div>
         <div>
-        <button className={style.button}><img src={pg} className={style.logo} /></button>
+          <button className={style.button}>
+            <img src={pg} className={style.logo} />
+          </button>
         </div>
         <div>
-        <button className={style.button}><img src={fg} className={style.logo} /></button>
+          <button className={style.button}>
+            <img src={fg} className={style.logo} />
+          </button>
         </div>
       </div>
     </div>
