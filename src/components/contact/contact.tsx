4b8622c4169c39334a20a945773fe0db5171b2bc
Deamond-Hero <leandrobrangi@gmail.com>
2024-05-02 13:11:29 -0300

    Fix bug form

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -6,12 +6,12 @@ import Swal from 'sweetalert2';
 
 
 export const Contact = () => {
-  
+
 
   const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
   const form = useRef<HTMLFormElement>(null);
   const [name, setName] = useState('')
-  const [email, setEmail] = useState('')  
+  const [email, setEmail] = useState('')
   const [motive, setMotive] = useState('')
   const [message, setMessage] = useState('')
   const [isVisible, setIsVisible] = useState(false)
@@ -22,44 +22,54 @@ export const Contact = () => {
     message: ""
   })
 
-  useEffect(()=>{
-    
-  },[name,email, motive, message])
+  useEffect(() => {
+
+  }, [name, email, motive, message])
 
   useEffect(() => {
     setTimeout(() => {
       setIsVisible(false)
     }, 3500);
     setIsVisible(true)
-  },[error])
-  
+  }, [error])
+
   const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-  
+
     if (form.current) {
+
+      let hasError = false;
+
       if (name === '') {
         setError(prevState => ({ ...prevState, name: 'Por favor ingresa tu nombre' }));
+        hasError = true;
       } else {
         setError(prevState => ({ ...prevState, name: '' }));
       }
       if (email === '') {
         setError(prevState => ({ ...prevState, email: 'Por favor ingresa tu correo' }));
+        hasError = true;
       } else if (!emailRegex.test(email)) {
         setError(prevState => ({ ...prevState, email: 'Por favor ingresa un correo válido' }));
+        hasError = true;
       } else {
         setError(prevState => ({ ...prevState, email: '' }));
       }
       if (motive === '') {
         setError(prevState => ({ ...prevState, motive: 'Por favor ingresa un motivo' }));
+        hasError = true;
       } else {
         setError(prevState => ({ ...prevState, motive: '' }));
       }
       if (message.length <= 20) {
         setError(prevState => ({ ...prevState, message: 'Ingresa un mensaje superior a 20 caracteres' }));
+        hasError = true;
       } else {
         setError(prevState => ({ ...prevState, message: '' }));
+        hasError = false;
       }
-      if (error.email.length === 0 && error.name.length === 0 && error.motive.length === 0 && error.message.length === 0) {
+      if (error.email === "" && error.name === "" && error.motive === "" && error.message === "") {
+        if (!hasError){
         try {
           emailjs
             .sendForm('service_3s6lweq', 'template_tfk9iwz', form.current, {
@@ -81,11 +91,12 @@ export const Contact = () => {
         } catch {
           console.log('Ouch! Algo salió mal');
         }
-      }else{
+      } else {
         console.log('Ouch! Algo salía mal');
       }
     }
-  };
+  }
+}
 
   return (
     <div className={style.container}>
@@ -105,22 +116,22 @@ export const Contact = () => {
           <div className={style.input}>
             <p>Nombre</p>
             <input type="text" name="user_name" value={name} onChange={(e) => setName(e.target.value)}></input>
-           { isVisible && error.name ?  <div className={style.error}>{error.name}</div> : null}
+            {isVisible && error.name ? <div className={style.error}>{error.name}</div> : null}
           </div>
           <div className={style.input}>
             <p>Email</p>
             <input type="email" name="user_email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-            { isVisible && error.email ?  <div className={style.error}>{error.email}</div> : null}
+            {isVisible && error.email ? <div className={style.error}>{error.email}</div> : null}
           </div>
           <div className={style.input}>
             <p>Motivo</p>
             <input value={motive} onChange={(e) => setMotive(e.target.value)}></input>
-            { isVisible && error.motive ?  <div className={style.error}>{error.motive}</div> : null}
+            {isVisible && error.motive ? <div className={style.error}>{error.motive}</div> : null}
           </div>
           <div className={style.message}>
             <p>Mensaje</p>
             <textarea name="message" maxLength={250} value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
-            { isVisible && error.message ?  <div className={style.error}>{error.message}</div> : null}
+            {isVisible && error.message ? <div className={style.error}>{error.message}</div> : null}
           </div>
           <div className={style.send}>
             <button type="submit" value="Send">Enviar</button>
